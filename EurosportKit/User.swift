efecad53fa6dc63be32c8d04de69feec9d86ae67
Alexander Edge <alex@alexedge.co.uk>
2016-06-03 12:37:12 +0100

    Add missing User source

--- EurosportKit/User.swift
+++ EurosportKit/User.swift
@@ -1,6 +1,33 @@
 import Foundation
+import CoreData
 
 @objc(User)
-public class User: _User {
-	// Custom logic goes here.
+public class User: NSManagedObject {
+    
+    @NSManaged public var identifier: String
+    @NSManaged public var hkey: String
+    @NSManaged public var email: String
+    
+}
+
+extension User: StringIdentifiable { }
+extension User: Fetchable {
+
+    public typealias FetchableType = User
+    
+    public static func entityName() -> String {
+        return "User"
+    }
+}
+
+extension User {
+    
+    public static func currentUser(context: NSManagedObjectContext) -> User? {
+        do {
+            return try singleObjectInContext(context, predicate: nil, sortedBy: nil, ascending: true)
+        } catch {
+            return nil
+        }
+    }
+    
 }

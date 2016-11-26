6e3d6897d9de58db5a5ff54a4982533b931607dd
Alexander Edge <alex@alexedge.co.uk>
2016-11-26 18:05:52 +0000

    Use save extension on moc

--- EurosportPlayerTV/AppDelegate.swift
+++ EurosportPlayerTV/AppDelegate.swift
@@ -47,7 +47,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        self.saveContext()
+        saveContext()
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -61,7 +61,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        self.saveContext()
+        saveContext()
     }
 
     private lazy var applicationCachesDirectory: URL = {
@@ -81,17 +81,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
     
     private func saveContext () {
-        if managedObjectContext.hasChanges {
-            do {
-                try managedObjectContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-                abort()
-            }
-        }
+        managedObjectContext.saveToPersistentStore()
     }
     
 }

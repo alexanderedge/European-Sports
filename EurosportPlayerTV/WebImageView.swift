14eca101c880f2f337be3ada525971dda4c84ce7
Alexander Edge <alex@alexedge.co.uk>
2016-12-19 20:46:03 +0000

    Use URLCache to check for existing image

---
+++ EurosportPlayerTV/WebImageView.swift
@@ -0,0 +1,89 @@
+//
+//  WebImageView.swift
+//  EurosportPlayer
+//
+//  Created by Alexander Edge on 28/05/2016.
+//  Copyright © 2016 Alexander Edge Ltd. All rights reserved.
+//
+
+import UIKit
+
+fileprivate struct DarkenFilter {
+    
+    internal var filter: (UIImage) -> UIImage {
+        return { image in
+            return UIImageEffects.imageByApplyingBlur(to: image, withRadius: 0, tintColor: UIColor.black.withAlphaComponent(0.6), saturationDeltaFactor: 1, maskImage: nil)
+        }
+    }
+}
+
+internal class WebImageView: UIImageView {
+    
+    private var currentTask: URLSessionTask?
+    
+    func setImage(_ url: URL?, placeholder: UIImage? = nil, darken: Bool) {
+        
+        let filter = DarkenFilter()
+        
+        if let placeholder = placeholder {
+            self.image = placeholder
+        }
+        
+        if let url = url {
+            
+            // check for a cached response
+            if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)), let cachedImage = UIImage(data: cachedResponse.data) {
+                self.image = darken ? filter.filter(cachedImage) : cachedImage
+            } else {
+                
+                let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                    
+                    if let data = data {
+                        
+                        guard let image = UIImage(data: data) else {
+                            return
+                        }
+                        
+                        if darken {
+                            
+                            let filteredImage = filter.filter(image)
+                            
+                            DispatchQueue.main.async {
+                                self.setImageWithTransition(image: filteredImage, animate: true)
+                            }
+                            
+                            
+                        } else {
+                            DispatchQueue.main.async {
+                                self.setImageWithTransition(image: image, animate: true)
+                            }
+                        }
+                        
+                    }
+                    
+                }
+                
+                currentTask = task
+                task.resume()
+                
+            }
+            
+        }
+
+    }
+    
+    func cancelCurrentImageLoad() {
+        currentTask?.cancel()
+    }
+    
+    private func setImageWithTransition(image: UIImage, animate: Bool) {
+        self.alpha = 0
+        self.image = image
+        UIView.transition(with: self, duration: (animate ? 0.2 : 0), options: .transitionCrossDissolve, animations: {
+            self.alpha = 1
+        }, completion: nil)
+    }
+    
+}
+
+
